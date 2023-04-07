c365c0de36b8233be5829a31bcbc63df4e4b728d
Steven Salor <ssalor@k0r4p11.42mulhouse.fr>
2023-04-07 14:39:51 +0200

    4

---
+++ so_long/includes/tiles.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tiles.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssalor <ssalor@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/07 14:20:59 by ssalor            #+#    #+#             */
+/*   Updated: 2023/04/07 14:27:44 by ssalor           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TILES_H
+# define TILES_H
+
+# define WALL_TILE "../tiles/"
+# define FLOOR_TILE "../tiles/"
+# define PLAYER_TILE "../tiles/New-Piskel.xpm42"
+//# define ENEMY_TILE "../tiles/"
+# define COLLECTIBLE_TILE "../tiles/"
+# define EXIT_TILE "../tiles/"
+
+# define TILE_SIZE 64
+
+#endif
